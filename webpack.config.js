9de9eb72c061a324104c7a3076994ae872db3372
sam-xif <s.xifaras999@gmail.com>
2019-11-19 18:45:14 -0500

    Added development mode to webpack and passing of currentPath and allPaths to the onPathFinish callback

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './lib',
+  mode: 'development',
   output: {
       path: path.resolve(__dirname, './dist'),
       filename: 'index.js',
